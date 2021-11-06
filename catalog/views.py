dad6988e1407370541765f3ed749b00e04d79433
nickelk <nick_smith@ukr.net>
2021-11-06 21:22:09 +0200

    index page added

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_genres = Genre.objects.all().count()
+    num_books_word_in_title = Book.objects.filter(title__icontains='adventures').count()
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books,
+                 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available,
+                 'num_authors': num_authors,
+                 'num_genres': num_genres,
+                 'num_books_word_in_title': num_books_word_in_title,
+                 },
+    )
