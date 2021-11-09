c34ed104956e9e0270302e154e1efb6ee9143503
nickelk <nick_smith@ukr.net>
2021-11-09 21:14:15 +0200

    renewal form added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-# from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     url(r'^$', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
@@ -11,4 +9,5 @@ urlpatterns = [
     url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     url(r'^allbooksloaned/$', views.LoanedBooksByAllListView.as_view(), name='all-books-loaned'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
